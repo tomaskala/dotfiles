ce416748df52a572b3b82e65ae6ba5019030e617
Tomas Kala <me@tomaskala.com>
2019-08-06 13:54:51 +0200

    Command to cd & immediately ls

--- aliases.zsh
+++ aliases.zsh
@@ -4,3 +4,7 @@ alias vim="nvim"
 alias vi="nvim"
 alias cls="clear"
 alias upgrade-kitty="curl -L https://sw.kovidgoyal.net/kitty/installer.sh | sh /dev/stdin"
+
+function cc {
+    cd "$1" && ll
+}
