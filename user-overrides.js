20eefb9a2e210a8b792bd57bb589c09fe5ddee42
Tomas Kala <me@tomaskala.com>
2019-09-24 11:17:54 +0200

    Cleaned up the user-overrides.js

--- user-overrides.js
+++ user-overrides.js
@@ -1,43 +1,54 @@
+// Restore last session on startup.
+user_pref("browser.startup.page", 3);  // 0102
+
+// Use the 'about:home' page when opening a new tab.
+user_pref("browser.startup.homepage", "about:home");  // 0103
+user_pref("browser.newtabpage.enabled", true);  // 0104
+user_pref("browser.newtab.preload", true);
+
+// Do not start the browser in private mode.
+user_pref("browser.privatebrowsing.autostart", false);  // 0110
+
+// Enable real time binary checks with Google services.
 user_pref("browser.safebrowsing.downloads.remote.enabled", true);  // 0402
 
+// Make the URL bar work as a search bar again -- allow if you use a privacy
+// respecting search engine.
+user_pref("keyword.enabled", true);  // 0801
+
+// Enable disk cache.
+user_pref("browser.cache.disk.enable", true);  // 1001
+
+// Enable storing extra session data such as the scrollbar positions.
+user_pref("browser.sessionstore.privacy_level", 0);  // 1021
+
+// Reset the minimum time interval between session save operations.
+user_pref("browser.sessionstore.interval", 15000);  // 1023
+
+// Allow webpages to push their own fonts.
+user_pref("browser.display.use_document_fonts", 1);  // 1401
+
+// Enable WebGL.
 user_pref("webgl.disabled", false);  // 2010
 user_pref("webgl.dxgl.enabled", true); // [WINDOWS]
 user_pref("webgl.enable-webgl2", true);
 
+// Enable webpages to register clipboard events. Otherwise, Jupyter and
+// Zeppelin notebooks do not work correctly.
 user_pref("dom.event.clipboardevents.enabled", true);  // 2402
 
-user_pref("browser.cache.disk.enable", true);  // 1001
+// Do not clear everything on shutdown.
+user_pref("privacy.clearOnShutdown.cookies", false);  // 2803
+user_pref("privacy.clearOnShutdown.sessions", false);
+user_pref("privacy.clearOnShutdown.siteSettings", false);
 
-user_pref("keyword.enabled", true);  // 0801
+// Autoselect the same settings as 2803 when pressing Ctrl+Shift+Del.
+user_pref("privacy.cpd.cookies", false);  // 2804
+user_pref("privacy.cpd.sessions", false);
+user_pref("privacy.cpd.siteSettings", false);
 
-user_pref("browser.sessionstore.privacy_level", 0);  // 1021
+// Disabled because of cookie autodelete.
+user_pref("privacy.firstparty.isolate", false);  // 4001
 
-user_pref("browser.sessionstore.interval", 15000);  // 1023
-
+// Do not show the annoying white frame around webpages.
 user_pref("privacy.resistFingerprinting.letterboxing", false);  // 4504
-
-user_pref("browser.startup.page", 3);  // 0102
-
-user_pref("browser.privatebrowsing.autostart", false);  // 0110
-
-user_pref("browser.display.use_document_fonts", 1);  // 1401
-
-user_pref("privacy.clearOnShutdown.cache", true);  // 2803
-user_pref("privacy.clearOnShutdown.cookies", false);
-user_pref("privacy.clearOnShutdown.downloads", true); // see note above
-user_pref("privacy.clearOnShutdown.formdata", false); // Form & Search History
-user_pref("privacy.clearOnShutdown.history", false); // Browsing & Download History
-user_pref("privacy.clearOnShutdown.offlineApps", true); // Offline Website Data
-user_pref("privacy.clearOnShutdown.sessions", false); // Active Logins
-user_pref("privacy.clearOnShutdown.siteSettings", false); // Site Preferences
-
-user_pref("privacy.cpd.cache", true);  // 2804
-user_pref("privacy.cpd.cookies", false);
-user_pref("privacy.cpd.formdata", false); // Form & Search History
-user_pref("privacy.cpd.history", false); // Browsing & Download History
-user_pref("privacy.cpd.offlineApps", true); // Offline Website Data
-user_pref("privacy.cpd.passwords", true); // this is not listed
-user_pref("privacy.cpd.sessions", false); // Active Logins
-user_pref("privacy.cpd.siteSettings", false); // Site Preferences
-
-user_pref("privacy.firstparty.isolate", false);  // 4001; disabled because of cookie autodelete
