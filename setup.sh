de694a958bf70f935c317e162dfefe1957b48691
Tomas Kala <me@tomaskala.com>
2021-02-03 19:36:21 +0100

    Correct setup script

--- setup.sh
+++ setup.sh
@@ -1,34 +1,22 @@
 #!/bin/bash
-set -euf -o pipefail
+set -eu -o pipefail
+GLOBIGNORE=".:..:.git"
 
 
-function set_ln {
-  what="$1"
-  where="$2"
+function install_dotfile {
+  dotfile="$1"
+  dest="$HOME/${dotfile}"
 
-  if [ -f "$where" ]; then
-    rm "$where"
-  elif [ -d "$where" ]; then
-    rm -r "$where"
-  fi
+  echo "Installing ${dotfile}"
 
-  ln -s "$what" "$where"
+  mkdir -p -m 700 "$(dirname "${dest}")"
+  chmod go-rwx "${dotfile}"
+  ln -fs "$(pwd)/${dotfile}" "${dest}"
 }
 
-root_dir="$(realpath "$(dirname "${BASH_SOURCE[0]}")")"
 
-set_ln "${root_dir}/.bashrc" "$HOME/.bashrc"
-set_ln "${root_dir}/.gitconfig" "$HOME/.gitconfig"
-set_ln "${root_dir}/.tmux.conf" "$HOME/.tmux.conf"
-set_ln "${root_dir}/.config/nvim" "$HOME/.config/nvim"
-set_ln "${root_dir}/.wallpaper" "$HOME/.wallpaper"
-
-mkdir -p "$HOME/.fonts"
-set_ln "${root_dir}/.fonts/fontawesome.ttf" "$HOME/.fonts/fontawesome.ttf"
-
-mkdir -p "$HOME/.local/bin"
-
-while read -r script; do
-  script_name=$(basename "${script}")
-  set_ln "${root_dir}/bin/${script_name}" "$HOME/.local/bin/${script_name}"
-done < <(find "${root_dir}/bin" -type f -executable)
+for source in .*; do
+  while read -r dotfile; do
+    install_dotfile "${dotfile}"
+  done < <(find "${source}" -type f | sort)
+done
