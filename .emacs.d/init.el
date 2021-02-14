f34014a0161f36217d8638bf1dd0e8c337484092
Tomas Kala <me@tomaskala.com>
2021-02-14 22:08:19 +0100

    Emacs tweaks

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -42,11 +42,22 @@
 ;; Show matching parenthesis.
 (show-paren-mode 1)
 
+;; Highlight the current line.
+(require 'hl-line)
+(add-hook 'prog-mode-hook #'hl-line-mode)
+(add-hook 'text-mode-hook #'hl-line-mode)
+
 ;; Shorter yes/no.
 (defalias 'yes-or-no-p 'y-or-n-p)
 
 ;; Force UTF-8.
-(prefer-coding-system 'utf-8-unix)
+(set-charset-priority 'unicode)
+(setq locale-coding-system 'utf-8)
+(set-terminal-coding-system 'utf-8)
+(set-keyboard-coding-system 'utf-8)
+(set-selection-coding-system 'utf-8)
+(prefer-coding-system 'utf-8)
+(setq default-process-coding-system '(utf-8-unix . utf-8-unix))
 
 ;; Disable built-in version control tools.
 (setq vc-display-status nil
