0ccdeaa33003857c3850cab440cbebd934799160
Tomas Kala <me@tomaskala.com>
2021-03-20 01:19:10 +0100

    Install private (encrypted) files

--- setup.sh
+++ setup.sh
@@ -1,6 +1,17 @@
 #!/bin/bash
 set -eu -o pipefail
 GLOBIGNORE=".:..:.git:.gitignore"
+INSTALL_PRIVATE=false
+
+GPG="gpg"
+which gpg2 &>/dev/null && GPG="gpg2"
+
+
+__USAGE="Usage: $(basename "$0") [OPTIONS]
+
+Options:
+  -p  Install private (encrypted) files.
+  -h  Show this message and exit."
 
 
 function install_dotfile {
@@ -15,9 +26,37 @@ function install_dotfile {
 }
 
 
+function decrypt_dotfile {
+  dotfile="$1"
+  dest="${HOME}/${dotfile%.gpg}"
+
+  if [[ ! -e "${dest}" || "${dotfile}" -nt "${dest}" ]]; then
+    echo "Decrypting ${dotfile}"
+    mkdir -p -m 700 "$(dirname "${dest}")"
+    (umask 0177;
+    "${GPG}" --quiet -o "${dest}" -d "${dotfile}")
+  else
+    echo "Skipping ${dotfile}"
+  fi
+}
+
+
+while getopts "hp" arg; do
+  case "${arg}" in
+    h) echo "${__USAGE}"; exit 0 ;;
+    p) INSTALL_PRIVATE=true ;;
+    *) echo "${__USAGE}"; exit 1 ;;
+  esac
+done
+
+
 for source in .*; do
   while read -r dotfile; do
-    install_dotfile "${dotfile}"
+    if [[ "${dotfile}" = "${dotfile##*.gpg}" ]]; then
+      install_dotfile "${dotfile}"
+    elif [[ "${INSTALL_PRIVATE}" = true ]]; then
+      decrypt_dotfile "${dotfile}"
+    fi
   done < <(find "${source}" -type f | sort)
 done
 
