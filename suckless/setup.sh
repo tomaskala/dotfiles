fa7e65133491e43dcb67630010866e64db82065d
Tomas Kala <me@tomaskala.com>
2021-06-12 13:21:50 +0200

    Move suckless out of home

--- suckless/setup.sh
+++ suckless/setup.sh
@@ -27,23 +27,25 @@ if ! type make > /dev/null; then
   die "Make is not installed."
 fi
 
-safeclone "https://git.suckless.org/dwm" "${HOME}/dwm"
-ln -fs "$(pwd)/dwm/config.h" "${HOME}/dwm"
-sudo make -C "${HOME}/dwm" clean install
+mkdir -p "${HOME}/suckless"
 
-safeclone "https://git.suckless.org/dmenu" "${HOME}/dmenu"
-sudo make -C "${HOME}/dmenu" clean install
+safeclone "https://git.suckless.org/dwm" "${HOME}/suckless/dwm"
+ln -fs "$(pwd)/dwm/config.h" "${HOME}/suckless/dwm"
+sudo make -C "${HOME}/suckless/dwm" clean install
 
-safeclone "https://git.suckless.org/slstatus" "${HOME}/slstatus"
-ln -fs "$(pwd)/slstatus/config.h" "${HOME}/slstatus"
-sudo make -C "${HOME}/slstatus" clean install
+safeclone "https://git.suckless.org/dmenu" "${HOME}/suckless/dmenu"
+sudo make -C "${HOME}/suckless/dmenu" clean install
 
-safeclone "https://git.suckless.org/st" "${HOME}/st"
-ln -fs "$(pwd)/st/config.h" "${HOME}/st"
-sudo make -C "${HOME}/st" clean install
+safeclone "https://git.suckless.org/slstatus" "${HOME}/suckless/slstatus"
+ln -fs "$(pwd)/slstatus/config.h" "${HOME}/suckless/slstatus"
+sudo make -C "${HOME}/suckless/slstatus" clean install
 
-safeclone "https://git.suckless.org/slock" "${HOME}/slock"
-sudo make -C "${HOME}/slock" clean install
+safeclone "https://git.suckless.org/st" "${HOME}/suckless/st"
+ln -fs "$(pwd)/st/config.h" "${HOME}/suckless/st"
+sudo make -C "${HOME}/suckless/st" clean install
+
+safeclone "https://git.suckless.org/slock" "${HOME}/suckless/slock"
+sudo make -C "${HOME}/suckless/slock" clean install
 
 ln -fs "$(pwd)/.xprofile" "${HOME}/.xprofile"
 ln -fs "$(pwd)/.xinitrc" "${HOME}/.xinitrc"
