aa74b8f3426901573aac514661f54d27b2d369a3
Tomas Kala <me@tomaskala.com>
2021-06-29 23:12:02 +0200

    Correctly set directory permissions

--- setup.sh
+++ setup.sh
@@ -20,10 +20,12 @@ command -v gpg2 > /dev/null && gpg="gpg2"
 install_dotfile() {
   dotfile="$1"
   dest="${HOME}/${dotfile}"
+  target_dir="$(dirname "${dest}")"
 
   printf 'Installing %s\n' "${dotfile}"
 
-  mkdir -p -m 700 "$(dirname "${dest}")"
+  mkdir -p "${target_dir}"
+  [ "${target_dir}" != "${HOME}" ] && chmod -R 700 "${target_dir}"
   chmod go-rwx "${dotfile}"
   ln -fs "$(pwd)/${dotfile}" "${dest}"
 }
@@ -36,7 +38,9 @@ decrypt_dotfile() {
   if [ ! -e "${dest}" ] || \
       [ -n "$(find -L "${dotfile}" -prune -newer "${dest}")" ]; then
     printf 'Decrypting %s\n' "${dotfile}"
-    mkdir -p -m 700 "$(dirname "${dest}")"
+    target_dir="$(dirname "${dest}")"
+    mkdir -p "${target_dir}"
+    [ "${target_dir}" != "${HOME}" ] && chmod -R 700 "${target_dir}"
     (umask 0177;
     "${gpg}" --quiet -o "${dest}" -d "${dotfile}")
   else
@@ -47,7 +51,8 @@ decrypt_dotfile() {
 
 install_notmuch_hooks() {
   printf 'Installing notmuch hooks\n'
-  mkdir -p -m 700 ~/Mail/.notmuch/hooks
+  mkdir -p ~/Mail/.notmuch/hooks
+  chmod -R 700 ~/Mail/.notmuch/hooks
 
   hook="$(pwd)/.local/hooks/notmuch-pre-new"
   chmod 700 "${hook}"
