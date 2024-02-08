8383c93878ca943dc0f6023a2b5a2b739236d909
Tomas Kala <me@tomaskala.com>
2024-02-08 14:23:58 +0100

    Configure telescope.nvim

--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -163,26 +163,64 @@ require("lazy").setup({
           vim.bo[args.buf].omnifunc = "v:lua.vim.lsp.omnifunc"
 
           -- Buffer local mappings.
-          local opts = { buffer = args.buf, noremap = true, silent = true }
-          vim.keymap.set("n", "gD", vim.lsp.buf.declaration, opts)
-          vim.keymap.set("n", "gd", vim.lsp.buf.definition, opts)
-          vim.keymap.set("n", "K", vim.lsp.buf.hover, opts)
-          vim.keymap.set("n", "gi", vim.lsp.buf.implementation, opts)
-          vim.keymap.set("n", "<C-k>", vim.lsp.buf.signature_help, opts)
-          vim.keymap.set("n", "<leader>wa", vim.lsp.buf.add_workspace_folder, opts)
-          vim.keymap.set("n", "<leader>wr", vim.lsp.buf.remove_workspace_folder, opts)
+          local keymap_opts = { buffer = args.buf, noremap = true, silent = true }
+          vim.keymap.set("n", "gD", vim.lsp.buf.declaration, keymap_opts)
+          vim.keymap.set("n", "gd", vim.lsp.buf.definition, keymap_opts)
+          vim.keymap.set("n", "K", vim.lsp.buf.hover, keymap_opts)
+          vim.keymap.set("n", "gi", vim.lsp.buf.implementation, keymap_opts)
+          vim.keymap.set("n", "<C-k>", vim.lsp.buf.signature_help, keymap_opts)
+          vim.keymap.set("n", "<leader>wa", vim.lsp.buf.add_workspace_folder, keymap_opts)
+          vim.keymap.set("n", "<leader>wr", vim.lsp.buf.remove_workspace_folder, keymap_opts)
           vim.keymap.set("n", "<leader>wl", function()
             print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
-          end, opts)
-          vim.keymap.set("n", "<leader>D", vim.lsp.buf.type_definition, opts)
-          vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, opts)
-          vim.keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, opts)
-          vim.keymap.set("n", "gr", vim.lsp.buf.references, opts)
+          end, keymap_opts)
+          vim.keymap.set("n", "<leader>D", vim.lsp.buf.type_definition, keymap_opts)
+          vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, keymap_opts)
+          vim.keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, keymap_opts)
+          vim.keymap.set("n", "gr", vim.lsp.buf.references, keymap_opts)
           vim.keymap.set("n", "<leader>f", function()
             vim.lsp.buf.format({ async = true })
-          end, opts)
+          end, keymap_opts)
         end,
       })
+    end,
+  },
+  {
+    "nvim-telescope/telescope.nvim",
+    dependencies = { "nvim-lua/plenary.nvim" },
+    opts = {
+      defaults = {
+        file_ignore_patterns = {
+          ".git",
+        },
+        vimgrep_arguments = {
+          "ag",
+          "--hidden",
+          "--no-color",
+          "--no-heading",
+          "--filename",
+          "--numbers",
+          "--column",
+          "--smart-case",
+        },
+      },
+      pickers = {
+        find_files = {
+          hidden = true,
+        },
+      },
+    },
+    config = function(_, opts)
+      local builtin = require("telescope.builtin")
+      local keymap_opts = { noremap = true }
+      vim.keymap.set("n", "<leader>ff", builtin.find_files, keymap_opts)
+      vim.keymap.set("n", "<leader>fg", builtin.live_grep, keymap_opts)
+      vim.keymap.set("n", "<leader>fb", builtin.buffers, keymap_opts)
+      vim.keymap.set("n", "<leader>fh", builtin.help_tags, keymap_opts)
+      vim.keymap.set("n", "<leader>fr", builtin.lsp_references, keymap_opts)
+      vim.keymap.set("n", "<leader>ft", builtin.treesitter, keymap_opts)
+
+      require("telescope").setup(opts)
     end,
   },
 })
