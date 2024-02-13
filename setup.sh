9aa8005cb32ca2f0e15ee254aeefebbffd5d6ec5
Tomas Kala <me@tomaskala.com>
2024-02-13 14:13:09 +0100

    Remove unused parts of the setup script

--- setup.sh
+++ setup.sh
@@ -1,14 +1,6 @@
 #!/bin/sh
 set -eu
 
-usage="Usage: $(basename "$0") [OPTIONS]
-
-Options:
-  -p  Install private (encrypted) files.
-  -h  Show this message and exit."
-
-install_private=false
-
 install_dotfile() {
   dotfile="$1"
   dest="${HOME}/${dotfile}"
@@ -20,29 +12,6 @@ install_dotfile() {
   ln -fs "$(pwd)/${dotfile}" "${dest}"
 }
 
-decrypt_dotfile() {
-  dotfile="$1"
-  dest="${HOME}/${dotfile%.gpg}"
-
-  if [ ! -e "${dest}" ] || \
-      [ -n "$(find -L "${dotfile}" -prune -newer "${dest}")" ]; then
-    printf 'Decrypting %s\n' "${dotfile}"
-    mkdir -p "$(dirname "${dest}")"
-
-    (umask 0177; gpg --quiet -o "${dest}" -d "${dotfile}")
-  else
-    printf 'Skipping %s\n' "${dotfile}"
-  fi
-}
-
-while getopts "hnp" arg; do
-  case "${arg}" in
-    h) printf '%s\n' "${usage}"; exit 0 ;;
-    p) install_private=true ;;
-    *) printf '%s\n' "${usage}"; exit 1 ;;
-  esac
-done
-
 for dotfiles_source in .*; do
   if [ "${dotfiles_source}" = '.' ] \
     || [ "${dotfiles_source}" = '..' ] \
@@ -51,11 +20,7 @@ for dotfiles_source in .*; do
   fi
 
   find "${dotfiles_source}" -type f | sort | while read -r dotfile; do
-    if [ "${dotfile}" = "${dotfile##*.gpg}" ]; then
-      install_dotfile "${dotfile}"
-    elif [ "${install_private}" = true ]; then
-      decrypt_dotfile "${dotfile}"
-    fi
+    install_dotfile "${dotfile}"
   done
 done
 
