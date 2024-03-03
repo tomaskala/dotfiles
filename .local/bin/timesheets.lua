839d48d19e3d502208724aa060f9d57c1411c7c9
Tomas Kala <me@tomaskala.com>
2024-03-03 17:08:01 +0100

    timesheets.lua: Add support for adding entries

--- .local/bin/timesheets.lua
+++ .local/bin/timesheets.lua
@@ -166,7 +166,7 @@ local function read_csv(lines)
   return data
 end
 
-local function write_csv(f, header, timesheet)
+local function to_csv(t)
   local function escape_csv(s)
     if string.find(s, '[,"]') then
       s = '"' .. string.gsub(s, '"', '""') .. '"'
@@ -174,14 +174,14 @@ local function write_csv(f, header, timesheet)
     return s
   end
 
-  local function to_csv(t)
-    local s = ""
-    for _, p in ipairs(t) do
-      s = s .. "," .. escape_csv(p)
-    end
-    return string.sub(s, 2)
+  local s = ""
+  for _, p in ipairs(t) do
+    s = s .. "," .. escape_csv(p)
   end
+  return string.sub(s, 2)
+end
 
+local function write_csv(f, header, timesheet)
   f:write(to_csv(header) .. "\n")
   for _, day_entry in ipairs(timesheet) do
     local row = {}
@@ -193,10 +193,66 @@ local function write_csv(f, header, timesheet)
 end
 
 local function parse_args()
-  local options = {work_start = 7, work_end = 18, max_hours = 10}
   local help = string.format([[
+Utility for the filling and randomization of a work timesheet.
+Usage: %s <command> <command-arguments> [command-options]
+Commands:
+  add        Add an entry to a work timesheet.
+  randomize  Randomize working hours of a work timesheet.
+Options:
+  --help  Show this message and exit
+]], arg[0])
+
+  local function die(msg, status)
+    io.stderr:write(msg)
+    os.exit(status)
+  end
+
+  local function parse_add(args)
+    local options = {date = os.date("%d/%m/%Y"), hours = 8}
+    local add_help = string.format([[
+Add an entry to a work timesheet.
+Usage: %s add <timesheet.csv> <description> [options]
+Arguments:
+  timesheet.csv         CSV file with columns 'Date', 'Hours' and 'Description'
+                        If it doesn't exist or is empty, it will be created.
+  description           String description of the work done on that day.
+Options:
+  --help                Show this message and exit.
+  --date   <DD/MM/YYYY> Date of the entry being added (default: %s).
+  --hours  <number>     Number of hours worked that day (default: %d).
+]],
+      args[0],
+      options["date"],
+      options["hours"]
+    )
+
+    if #args < 2 then
+      if args[1] == "--help" then die(add_help, 0) end
+      die(add_help, 1)
+    end
+    local timesheet_path = table.remove(args, 1)
+    local description = table.remove(args, 1)
+
+    for i = 1, #args, 2 do
+      local option = args[i]
+      if option == "--date" then
+        options["date"] = args[i + 1]:match("%d%d/%d%d/%d%d%d%d") or die(add_help, 1)
+      elseif option == "--hours" then
+        options["hours"] = tonumber(args[i + 1]) or die(add_help, 1)
+      else
+        die(add_help, 1)
+      end
+    end
+
+    return {timesheet_path, description}, options
+  end
+
+  local function parse_randomize(args)
+    local options = {work_start = 7, work_end = 18, max_hours = 10}
+    local randomize_help = string.format([[
 Randomize working hours of a work timesheet.
-Usage: %s <timesheet.csv> [options]
+Usage: %s randomize <timesheet.csv> [options]
 Arguments:
   timesheet.csv         CSV file with columns 'Date', 'Hours' and 'Description'
                         such that 'Date' contains DD/MM/YYYY, 'Hours' contains
@@ -204,72 +260,101 @@ Arguments:
                         empty 'Hours' or 'Description' imply a non-working day.
                         Alternatively, can be a "-" to denote stdin.
 Options:
-  --help                Show this message and exit
-  --start      <number> Work start on a given day (default: %d)
-  --end        <number> Work end on a given day (default: %d)
-  --max-hours  <number> Maximum working hours for a given day (default: %d)
+  --help                Show this message and exit.
+  --start      <number> Work start on a given day (default: %d).
+  --end        <number> Work end on a given day (default: %d).
+  --max-hours  <number> Maximum working hours for a given day (default: %d).
 ]],
-    arg[0],
-    options["work_start"],
-    options["work_end"],
-    options["max_hours"]
-  )
+      args[0],
+      options["work_start"],
+      options["work_end"],
+      options["max_hours"]
+    )
 
-  local function die(status)
-    io.stderr:write(help)
-    os.exit(status)
+    if #args == 0 then
+      die(randomize_help, 1)
+    elseif args[1] == "--help" then
+      die(randomize_help, 0)
+    end
+    local timesheet_path = table.remove(args, 1)
+
+    for i = 1, #args, 2 do
+      local option = args[i]
+      if option == "--start" then
+        options["work_start"] = tonumber(args[i + 1]) or die(randomize_help, 1)
+      elseif option == "--end" then
+        options["work_end"] = tonumber(args[i + 1]) or die(randomize_help, 1)
+      elseif option == "--max-hours" then
+        options["max_hours"] = tonumber(args[i + 1]) or die(randomize_help, 1)
+      else
+        die(randomize_help, 1)
+      end
+    end
+
+    return {timesheet_path}, options
   end
 
   if #arg == 0 then
-    die(1)
+    die(help, 1)
   elseif arg[1] == "--help" then
-    die(0)
-  end
-  local timesheet_path = table.remove(arg, 1)
-
-  for i = 1, #arg, 2 do
-    local option = arg[i]
-    if option == "--start" then
-      options["work_start"] = tonumber(arg[i + 1]) or die(1)
-    elseif option == "--end" then
-      options["work_end"] = tonumber(arg[i + 1]) or die(1)
-    elseif option == "--max-hours" then
-      options["max_hours"] = tonumber(arg[i + 1]) or die(1)
-    else
-      die(1)
-    end
+    die(help, 0)
   end
 
-  return timesheet_path, options
+  local command = table.remove(arg, 1)
+
+  if command == "add" then
+    return command, parse_add(arg)
+  elseif command == "randomize" then
+    return command, parse_randomize(arg)
+  else
+    die(help, 1)
+  end
 end
 
 local function main()
   local header = {"Date", "Start", "End", "Hours", "Description"}
-  local timesheet_path, options = parse_args()
-  local work_start = options["work_start"]
-  local work_end = options["work_end"]
-  local max_daily_hours = options["max_hours"]
-  local lines = timesheet_path == "-" and io.stdin:lines() or io.lines(timesheet_path)
+  local command, arguments, options = parse_args()
+  local timesheet_path = arguments[1]
 
-  local timesheet = read_csv(lines)
-  local work_days, free_days = split_timesheet(timesheet)
-  randomize_timesheet(work_days, work_start, work_end, max_daily_hours)
-  timesheet = finalize_timesheet(work_days, free_days)
-  write_csv(io.stdout, header, timesheet)
+  if command == "add" then
+    local description = arguments[2]
+    local date = options["date"]
+    local hours = options["hours"]
 
-  local total_hours = 0
-  local min_hours = math.maxinteger
-  local max_hours = math.mininteger
-  for _, entry in ipairs(timesheet) do
-    if is_work_day(entry) then
-      total_hours = total_hours + entry["Hours"]
-      min_hours = math.min(min_hours, entry["Hours"])
-      max_hours = math.max(max_hours, entry["Hours"])
+    local f = assert(io.open(timesheet_path, "a"))
+    if f:seek("end") == 0 then
+      f:write(to_csv({"Date", "Hours", "Description"}) .. "\n")
     end
+    f:write(to_csv({date, tostring(hours), description}) .. "\n")
+    f:close()
+  elseif command == "randomize" then
+    local work_start = options["work_start"]
+    local work_end = options["work_end"]
+    local max_daily_hours = options["max_hours"]
+    local lines = timesheet_path == "-" and io.stdin:lines() or io.lines(timesheet_path)
+
+    local timesheet = read_csv(lines)
+    local work_days, free_days = split_timesheet(timesheet)
+    randomize_timesheet(work_days, work_start, work_end, max_daily_hours)
+    timesheet = finalize_timesheet(work_days, free_days)
+    write_csv(io.stdout, header, timesheet)
+
+    local total_hours = 0
+    local min_hours = math.maxinteger
+    local max_hours = math.mininteger
+    for _, entry in ipairs(timesheet) do
+      if is_work_day(entry) then
+        total_hours = total_hours + entry["Hours"]
+        min_hours = math.min(min_hours, entry["Hours"])
+        max_hours = math.max(max_hours, entry["Hours"])
+      end
+    end
+    io.stderr:write(string.format("Total hours: %d\n", total_hours))
+    io.stderr:write(string.format("Min hours in a day: %0.1f\n", min_hours))
+    io.stderr:write(string.format("Max hours in a day: %0.1f\n", max_hours))
+  else
+    error(string.format("Unrecognized command: %s", command))
   end
-  io.stderr:write(string.format("Total hours: %d\n", total_hours))
-  io.stderr:write(string.format("Min hours in a day: %0.1f\n", min_hours))
-  io.stderr:write(string.format("Max hours in a day: %0.1f\n", max_hours))
 end
 
 main()
