fb41e34055b9f8302109c8da5971a2c00e7f2671
Tomas Kala <me@tomaskala.com>
2024-09-21 13:53:52 +0200

    Use command ls

--- .config/fish/functions/ls.fish
+++ .config/fish/functions/ls.fish
@@ -1,5 +1,3 @@
 function ls --description 'enhanced ls'
-  # Ugly hack to get the Nix-provided ls command, because the MacOS ls doesn't
-  # support some of the options I want.
-  /run/current-system/sw/bin/ls -FNh --color=auto --group-directories-first $argv
+  command ls -FNh --color=auto --group-directories-first $argv
 end
