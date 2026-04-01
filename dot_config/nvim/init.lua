6b59e1123b60ef518495d734ac4f64287ec8a1a3
Tomas Kala <me@tomaskala.com>
2026-04-01 20:15:51 +0200

    Update neovim config

--- dot_config/nvim/init.lua
+++ dot_config/nvim/init.lua
@@ -22,29 +22,32 @@ vim.opt.number = true
 vim.opt.relativenumber = true
 vim.opt.wildmode = { "longest:full", "full" }
 vim.opt.showmode = false
-vim.opt.completeopt = "menu,menuone,popup,fuzzy,noinsert"
+vim.opt.completeopt = { "menu", "menuone", "popup", "fuzzy", "noinsert" }
 vim.opt.winborder = "rounded"
 
 vim.opt.ignorecase = true
 vim.opt.smartcase = true
 
-vim.keymap.set("n", "<Esc>", function() vim.cmd("nohlsearch") end, { noremap = true })
+vim.keymap.set("n", "<Esc>", vim.cmd.nohlsearch, { noremap = true })
 vim.diagnostic.config({ virtual_text = true })
 
 vim.api.nvim_create_autocmd("FileType", {
   desc = "Go settings",
   pattern = "go",
-  group = vim.api.nvim_create_augroup("golang", { clear = true }),
+  group = vim.api.nvim_create_augroup("go", { clear = true }),
   callback = function()
+    vim.opt_local.tabstop = 4
+    vim.opt_local.softtabstop = 4
+    vim.opt_local.shiftwidth = 4
     vim.opt_local.expandtab = false
     vim.opt_local.makeprg = "go build"
   end,
 })
 
 vim.api.nvim_create_autocmd("FileType", {
-  desc = "Indent to 4 spaces",
-  pattern = { "go", "python" },
-  group = vim.api.nvim_create_augroup("indent_4_spaces", { clear = true }),
+  desc = "Python settings",
+  pattern = "python",
+  group = vim.api.nvim_create_augroup("python", { clear = true }),
   callback = function()
     vim.opt_local.tabstop = 4
     vim.opt_local.softtabstop = 4
@@ -57,10 +60,16 @@ vim.api.nvim_create_autocmd("FileType", {
   pattern = { "markdown", "text" },
   group = vim.api.nvim_create_augroup("plaintext", { clear = true }),
   callback = function()
-    vim.opt_local.textwidth = 79
-    vim.opt_local.formatoptions:append({ w = true })
-    vim.opt_local.tabstop = 2
-    vim.opt_local.softtabstop = 2
-    vim.opt_local.shiftwidth = 2
+    vim.opt_local.textwidth = 80
+    vim.opt_local.wrap = true
+    vim.opt_local.linebreak = true
+
+    vim.opt_local.spell = true
+    vim.opt_local.spelllang = "en_us"
+    vim.opt_local.formatoptions = "qn1j"
+
+    local opts = { buffer = true, noremap = true, silent = true }
+    vim.keymap.set({ "n", "x" }, "j", "gj", opts)
+    vim.keymap.set({ "n", "x" }, "k", "gk", opts)
   end,
 })
--- dot_config/nvim/lua/lsp.lua
+++ dot_config/nvim/lua/lsp.lua
@@ -2,8 +2,8 @@ vim.lsp.config("gopls", {
   cmd = { "gopls" },
   filetypes = { "go", "gomod", "gowork", "gotmpl", "gosum" },
   root_markers = {
-    "go.mod",
     "go.work",
+    "go.mod",
     ".git",
   },
 })
